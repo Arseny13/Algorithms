391298c025f12cf5e1671834183d50fd0c4cab52
Arseny13 <arsenyxiii@gmail.com>
2020-11-09 22:52:54 +0300

    gitignire

--- RK1/a_3.cpp
+++ RK1/a_3.cpp
@@ -5,18 +5,57 @@ using namespace std;
 using ll = long long;
 
 
+
+auto mult(vector<vector<ll>> & mat1, vector<vector<ll>> & mat2, ll m)
+{
+	auto dot = mat1;
+	for (int i = 0; i < 2; ++i)
+		for (int j = 0; j < 2; ++j)
+			dot[i][j] = ((mat1[i][0] * mat2[0][j]) % m + (mat1[i][1] * mat2[1][j]) % m) % m;
+
+	return dot;
+}
+
+
+
+
+ vector<vector<ll>> pow_mat(vector<vector<ll>> &mat, ll N,ll M){
+
+
+	if(N == 1)
+		return mat;
+
+	if(N & 1) {
+
+		auto b = pow_mat(mat, N - 1, M);
+		return mult(mat, b, M);
+	}
+
+	auto b = pow_mat(mat, N/2, M);
+	return mult(b, b, M);
+
+ }
+
+
+
+
+
+
 int main(){
-    ll  A0,A1,N, M,sum;
-    vector <long long> w(3,0);
+    ll  A0,A1,N, M;
+
+    vector <ll> w(2,0);
     cin >> A0 >> A1 >> N >> M;
     w[0]=A0%M;
     w[1]=A1%M;
-    for (ll i =2;i<=N;i++){
-        sum +=(w[0]+w[1])%M;
-        w[0]=w[1];
-        w[1] = sum;
-    }
-    cout << sum;
+    if (N==0) cout << w[0];
+    vector <vector<ll>> v{{0,1},{1,1}};
+    auto mat2 = pow_mat(v, N, M);
+
+	cout << ((mat2[0][0] * w[0]) % M + (mat2[0][1] * w[1]) % M) % M << endl;
+
+
+
     return 0;
 
 
--- RK1/a_4.cpp
+++ RK1/a_4.cpp
@@ -1,5 +1,5 @@
 
-#include <bits/stdc++.h>
+/*#include <bits/stdc++.h>
 #include <algorithm>
 using ll= long long;
 using namespace std;
@@ -65,3 +65,52 @@ int main()
 
 
 
+*/
+
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+int main()
+{
+
+	size_t n, m, a;
+
+	cin >> n >> m;
+	vector<size_t> mas(n);
+
+	for(size_t i = 0; i < n; ++i)
+		cin >> mas[i];
+
+	for(size_t i = 0; i < m; ++i)
+	{
+		cin >> a;
+		size_t sum = 0, l = 0, r = 0;
+
+		bool flag = false;
+
+		while(l < n && r < n)
+		{
+			if(sum == a)
+			{
+				cout << l + 1 << ' ' << r + 1 << endl;
+				flag = true;
+				break;
+			}
+
+			if(sum < a)
+				sum += mas[r++];
+			else if(sum > a)
+				sum -= mas[l++];
+
+		}
+
+		if(!flag)
+			cout << "Not found" << endl;
+
+	}
+
+	return 0;
+}
